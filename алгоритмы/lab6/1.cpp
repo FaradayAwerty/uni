fc7d8b705f922d99d45f04b39f1658ab6b76fa94
FaradayAwerty <faradaysquared@gmail.com>
2021-06-01 06:15:55 +0300

    -

---
+++ алгоритмы/lab6/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const string filename = "field.txt";
+
+int main()
+{
+	int m, n;
+	ifstream file_field("field.txt");
+
+
+
+	file_field.close();
+
+	return 0;
+}
+
