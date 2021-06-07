baa6c207e89b5f6076b2fb91709483a87d06d59a
FaradayAwerty <faradaysquared@gmail.com>
2021-06-07 14:49:12 +0300

    -

--- дискретная_математика/lab2/program.c
+++ дискретная_математика/lab2/program.c
@@ -7,7 +7,7 @@ void print_pascal_triangle(int n);
 int main()
 {
 	printf("%lld\n", binomial(40, 15));
-	print_pascal_triangle(10);
+	print_pascal_triangle(6);
 
 	return 0;
 }
@@ -29,6 +29,7 @@ long long int binomial(int n, int k)
 void print_pascal_triangle(int n)
 {
 	int *row_d, *row_u = (int *)malloc(sizeof(int) * 1);
+	row_u[0] = 1;
 
 	for(int i = 1; i < n + 1; i++) {
 		row_d = (int *)malloc(sizeof(int) * (i + 1));
@@ -39,13 +40,14 @@ void print_pascal_triangle(int n)
 			else
 				row_d[j] = row_u[j] + row_u[j - 1];
 
+		free(row_u);
 		row_u = row_d;
 	}
 
 	for(int i = 0; i < n + 1; i++)
-		printf("\t%d", row_u[i]);
+		printf("\t%d", row_d[i]);
 	putchar('\n');
 
-	free(row_u);
+	free(row_d);
 }
 
