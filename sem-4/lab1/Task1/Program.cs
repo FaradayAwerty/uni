126c23929ba9a0900284e8ce7fa67aae794ce4ef
FaradayAwerty <faradaysquared@gmail.com>
2022-02-10 12:41:17 +0300

    -

---
+++ sem-4/lab1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Task1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double x = Convert.ToDouble(Console.ReadLine());
+			double y = Convert.ToDouble(Console.ReadLine());
+			double z = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine( (x*x*x + Math.Sqrt(Math.Pow(z, 6) - 5*x*y))/Math.Abs(-7*x*x*Math.Pow(y, 8) + z) );
+		}
+	}
+}
+
---
+++ sem-4/lab1/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Task2
+{
+	class Program
+	{
+		// checks if d1 is before d2
+		static bool isBefore(DateTime d1, DateTime d2)
+		{
+			if(d1.Year != d2.Year)
+				return false;
+			else if(d1.Month != d2.Month)
+				return d1.Month < d2.Month;
+			else if(d1.Day != d2.Day)
+				return d1.Day < d2.Day;
+			return false;
+		}
+
+		static void Main(string[] args)
+		{
+			DateTime date1, date2;
+
+			try {
+				date1 = Convert.ToDateTime(Console.ReadLine());
+				date2 = Convert.ToDateTime(Console.ReadLine());
+			} catch(System.FormatException) {
+				Console.WriteLine("дата введена неправильно!");
+				return;
+			}
+
+
+			Console.WriteLine(date1);
+			Console.WriteLine(date2);
+
+			if(isBefore(date1, date2)) {
+				Console.WriteLine("в рамках года первая дата предшествует второй");
+			} else {
+				Console.WriteLine("в рамках года первая дата НЕ предшествует второй");
+			}
+		}
+	}
+}
+
