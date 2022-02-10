a9d62575c966a724b55f22807f1a32fa63592e39
FaradayAwerty <faradaysquared@gmail.com>
2022-02-10 13:15:26 +0300

    -

--- sem-4/lab1/Task3/Program.cs
+++ sem-4/lab1/Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Task3
+{
+	class Task3
+	{
+		static void Main(string[] args)
+		{
+			Int32 n = Convert.ToInt32(Console.ReadLine());
+			Int32[] arr = new Int32[n];
+
+			Random rnd = new Random();
+
+			for(int i = 0; i < n; i++) {
+				arr[i] = rnd.Next(0, 50);
+			}
+
+			for(int i = 0; i < n; i++) {
+				Console.WriteLine(arr[i]);
+			}
+		}
+	}
+}
+
