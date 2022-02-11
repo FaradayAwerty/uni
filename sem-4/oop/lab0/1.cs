56bf696cb2af0361fa6c62abaca9a1d9b96901d3
FaradayAwerty <faradaysquared@gmail.com>
2022-02-11 18:55:05 +0300

    -

---
+++ sem-4/oop/lab0/1.cs
@@ -0,0 +1,22 @@
+
+using System;
+
+public class MainClass {
+
+	public static void Main(string[] args) {
+		try {
+			Console.WriteLine(sum_of_squares(
+						Convert.ToInt32(args[0]),
+						Convert.ToInt32(args[1])));
+		} catch (System.IndexOutOfRangeException) {
+			Console.WriteLine(sum_of_squares(
+						Convert.ToInt32(Console.ReadLine()),
+						Convert.ToInt32(Console.ReadLine())));
+		}
+
+	}
+
+	public static Int32 sum_of_squares(Int32 a, Int32 b) {
+		return a*a + b*b;
+	}
+}
